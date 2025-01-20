96c418e69a82f97af87902447e58978d5989bc6f
dmittrj <dmittrj@outlook.com>
2025-01-20 17:46:56 +0300

    [JS] Add find_nearby_stop and get_time endpoints

--- js/kt-api.js
+++ js/kt-api.js
@@ -2,6 +2,7 @@
 /* https://github.com/kolomna-trams/api/ */
 
 class ktAPI {
+
     static async __kt_api_get(endpoint, params = "") {
         let response;
         try {
@@ -41,7 +42,7 @@ class ktAPI {
      * @param {string} [payment_type=null] - способ оплаты. Если параметр опущен, возвращаются тарифы для всех способов оплаты
      * @param {string} [passenger_type=null] - тип пассажиров. Если параметр опущен, возвращаются тарифы для всех типов пассажиров
      * @param {boolean} [pricing_type=false] - только тарификация. Если задано true, вывод будет содержать только информацию о тарифах (без внешней "обертки"), что позволит упростить ваш код
-     * @param {string} [key=null] key - ваш API-ключ
+     * @param {string} [key=null] - ваш API-ключ
      */
     static async get_fares(date = null, payment_type = null, passenger_type = null, pricing_type = false, key = null) {
         let params = [];
@@ -62,5 +63,45 @@ class ktAPI {
         }
 
         return await ktAPI.__kt_api_get("get_fares", params.join("&"));
+    }
+
+    /**
+     * __Ближайшая остановка__  
+     * Найти ближайшую трамвайную остановку по заданным координатам и получить информацию о ней.
+     * @param {number} lat - Широта. Выражена дробным числом. Для Коломны широта варьируется в диапазоне примерно 55.05...55.10
+     * @param {number} lon - Долгота. Выражена дробным числом. Для Коломны долгота варьируется в диапазоне примерно 38.75...38.80
+     * @param {boolean} code_only - Только код. Если задано true, вывод будет содержать только код остановки (без информации о ней), что может упростить ваш код
+     * @param {string} [key=null] - ваш API-ключ
+     */
+    static async find_nearby_stop(lat, lon, code_only = false, key = null) {
+        let params = [];
+        if (lat === undefined) {
+            throw new Error("Parameter `lat` is not set.");
+        }
+        params.push(`lat=${lat}`);
+        if (lon === undefined) {
+            throw new Error("Parameter `lat` is not set.");
+        }
+        params.push(`lon=${lon}`);
+        if (code_only) {
+            params.push(`code_only=${code_only}`);
+        }
+        if (key) {
+            params.push(`key=${key}`);
+        }
+        return await ktAPI.__kt_api_get("find_nearby_stop", params.join("&"));
+    }
+
+    /**
+     * __Время__  
+     * Получить время в Коломне (время часового пояса, в котором находится Коломна).
+     * @param {string} [key=null] - ваш API-ключ
+     */
+    static async get_time(key = null) {
+        let params = [];
+        if (key) {
+            params.push(`key=${key}`);
+        }
+        return await ktAPI.__kt_api_get("get_time", params.join("&"));
     }
 }
